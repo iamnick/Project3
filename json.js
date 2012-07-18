f3fbee2f88158d46547a5da61c721484d4dee1ef
Nick Stelzer <nickstelzer@Nicks-MacBook-Pro.local>
2012-07-18 16:10:09 -0400

    Created the JSON data.

--- json.js
+++ json.js
@@ -1,1 +1,24 @@
-alert("JavaScript works!");
+// Nick Stelzer
+// July 19, 2012
+// Project 3
+// JSON
+
+var json = {
+	"menuItems": [
+		{
+			"name": "Fish and Chips",
+			"price": 12.99,
+			"sideSalad": false
+		},
+		{
+			"name": "Baked Seafood Platter",
+			"price": 17.99,
+			"sideSalad": false
+		},
+		{
+			"name": "Chicken Parmigiana",
+			"price": 14.99,
+			"sideSalad": true
+		}
+	]
+};
--- stelzer_nick_assignment3.js
+++ stelzer_nick_assignment3.js
@@ -1,1 +1,19 @@
-alert("JavaScript works!");
+// Nick Stelzer
+// July 19, 2012
+// Project 3
+// Going out for dinner at Cap'n Jack's
+
+// Variables
+var restaurant = {
+	"name": "Cap'n Jack's",
+	"isOpen": true,
+	"menu": json.menuItems,
+	"menuItems": json.menuItems.length
+	}
+	
+	family = ["Papa", "Grandma", "Aunt Mary", "Aunt Sharon", "Mom", "Dad", "Nick", "Deanna", "Danielle"]
+;
+
+// Main Code
+console.log(restaurant.menu);
+console.log("The restaurant has " + restaurant.menuItems + " items on the menu.");
