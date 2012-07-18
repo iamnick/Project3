3dbf2abb04ae6afcb2fb7dafc43eece231920a31
Nick Stelzer <nickstelzer@Nicks-MacBook-Pro.local>
2012-07-18 17:14:04 -0400

    Added placeOrder Array Function.

--- stelzer_nick_assignment3.js
+++ stelzer_nick_assignment3.js
@@ -4,16 +4,41 @@
 // Going out for dinner at Cap'n Jack's
 
 // Variables
+var family = ["Papa", "Grandma", "Aunt Mary", "Aunt Sharon", "Mom", "Dad", "Nick", "Deanna", "Danielle"],
+	order = []
+;
+
 var restaurant = {
 	"name": "Cap'n Jack's",
 	"isOpen": true,
 	"menu": json.menuItems,
 	"menuItems": json.menuItems.length
 	}
-	
-	family = ["Papa", "Grandma", "Aunt Mary", "Aunt Sharon", "Mom", "Dad", "Nick", "Deanna", "Danielle"]
 ;
 
+var placeOrder = function (customers, menu) {
+	numOrders = customers.length;
+	o = 0;		// counter for # of orders taken
+	i = 0;   	// counter for item # on menu
+	order = [];	// empty array to store orders in
+	while (o < numOrders) {
+		order.push(menu[i].name);
+		i++;
+		if (i >= menu.length) {
+			i = 0;
+		}
+		o++;
+	}
+	return order;	// returns array of complete order 
+};
+
 // Main Code
-console.log(restaurant.menu);
-console.log("The restaurant has " + restaurant.menuItems + " items on the menu.");
+order = placeOrder(family, restaurant.menu);
+
+// Output
+for (var i = 0; i < family.length; i++) {
+	console.log(family[i] + " ordered the " + order[i] + ".");
+}
+
+//console.log(restaurant.menu);
+//console.log("The restaurant has " + restaurant.menuItems + " items on the menu.");
